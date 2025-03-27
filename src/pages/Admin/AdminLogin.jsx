54804ef43404f5d4f80adc6a26303daf9c804824
santhosh611 <santhoshsekar999@gmail.com>
2025-03-27 18:42:32 +0530

    mod:tailwind css

--- src/pages/Admin/AdminLogin.jsx
+++ src/pages/Admin/AdminLogin.jsx
@@ -45,7 +45,7 @@ const AdminLogin = () => {
   };
   
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="min-h-screen flex items-center justify-center bg-gray-300">
       <Card className="w-full max-w-md">
         <h1 className="text-2xl font-bold mb-6 text-center">Admin Login</h1>
         
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
+  content: ["./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",],
     theme: {
       extend: {
         colors: {
@@ -17,7 +15,7 @@ export default {
           light: '#f4f4f4',
           dark: '#333'
         },
-      },
-    },
-    plugins: [],
-  }
+      }},
+  plugins: [],
+}
+
