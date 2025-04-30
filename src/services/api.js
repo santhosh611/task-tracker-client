38aaba8d11784ba5d2b2266735c55f84c9fcc926
gdevakrishnan <gdevakrishnan700@gmail.com>
2025-04-30 21:38:59 +0530

    Notification move to production

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://task-tracker-backend-1-r8os.onrender.com/api',
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://task-tracker-backend-1-r8os.onrender.com/api',
+  // baseURL: 'http://localhost:5000/api',
   withCredentials: true, // If you need cookies for CORS, otherwise can remove
 });
 
